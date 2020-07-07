bcf9d173eb1caa7312e42da3aa20a2f43ede3a6a
ChrisBarno <67658251+ChrisBarno@users.noreply.github.com>
2020-07-07 12:37:18 +0200

    Delete 01_Datentypen_02.dart

--- 01_Datentypen_02.dart
+++
@@ -1,9 +0,0 @@
-main(){
-  String meinName = "Kiwi";
-  print("Der String ist $meinName.");
-  double zahl = 2.3;
-  String cast = zahl.toString();
-  print("Irgendeine Zahl: $cast.");
-  int laenge = meinName.length;
-  print("Die Länge des Strings ist $laenge.");
-}
